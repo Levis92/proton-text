86403d05041fb2798cccc871a6c0ce8cca8c551f
Anton Levholm <anton.badminton@gmail.com>
2017-05-02 16:55:25 +0200

    Add methods for buttons in GUI

--- src/main/java/edu/chl/proton/control/MainController.java
+++ src/main/java/edu/chl/proton/control/MainController.java
@@ -1,28 +1,20 @@
 package edu.chl.proton.control;
 
 import com.jfoenix.controls.JFXTabPane;
+import edu.chl.proton.model.DocumentType;
+import edu.chl.proton.model.Workspace;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
 
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Created by antonlevholm on 2017-05-01.
  */
-public class MainController implements Observer {
-    @Override
-    public void update(Observable o, Object arg) {
-
-    }
-
-    @FXML
-    public void test(ActionEvent event) throws IOException {
-        System.out.println("hej");
-    }
+public class MainController {
+    private Workspace workspace = Workspace.getInstance();
 
     @FXML
     private JFXTabPane tabPane;
@@ -32,5 +24,70 @@ public class MainController implements Observer {
         Tab tab = new Tab("Untitled");
         tab.setContent(loader.load());
         tabPane.getTabs().add(tab);
+    }
+
+    @FXML
+    public void onClickNewButton(ActionEvent event) throws IOException {
+        workspace.createDocument(DocumentType.MARKDOWN);
+    }
+
+    @FXML
+    public void onClickOpenButton(ActionEvent event) throws IOException {
+        workspace.openDocument(file);
+    }
+
+    @FXML
+    public void onClickSaveButton(ActionEvent) throws IOException {
+        workspace.saveCurrentDocument();
+    }
+
+    @FXML
+    public void onClickUndoButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickRedoButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickHeadingButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickBoldButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickItalicButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickQuoteButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickImageButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickCodeButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickOrderedListButton(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    public void onClickUnorderedListButton(ActionEvent event) throws IOException {
+
     }
 }
