273f04ac536f2b601f5ee1efca6bec1fcad7773e
Anton Levholm <anton.badminton@gmail.com>
2017-05-02 14:39:22 +0200

    Implemented simple methods in Workspace

--- src/main/java/edu/chl/proton/model/Workspace.java
+++ src/main/java/edu/chl/proton/model/Workspace.java
@@ -1,6 +1,9 @@
 package edu.chl.proton.model;
 
+import com.sun.tools.javac.code.Attribute;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Anton Levholm
@@ -8,17 +11,18 @@ import java.util.ArrayList;
  */
 public class Workspace {
     private static Workspace uniqueInstance = new Workspace();
-    /* List<Document> tabs = new ArrayList<Document>();
-    Document currentDocument;
-    Folder currentDirectory;
-    DocumentFactory factory; */
+    private List<Document> tabs = new ArrayList<Document>();
+    private Document currentDocument;
+    private Folder currentDirectory;
+    private DocumentFactory factory = new DocumentFactory();
+    private enum docType {MARKDOWN, PLAIN};
 
     private Workspace() {}
 
     public static Workspace getInstance() {
         return uniqueInstance;
     }
-/*
+
     public void setCurrentDocument(Document doc) {
         currentDocument = doc;
     }
@@ -27,7 +31,7 @@ public class Workspace {
         return currentDocument;
     }
 
-    public setCurrentDirectory(Folder folder) {
+    public void setCurrentDirectory(Folder folder) {
         currentDirectory = folder;
     }
 
@@ -35,20 +39,21 @@ public class Workspace {
         return currentDirectory;
     }
 
-    public void createDocument(File file) {
-
+    public void createDocument(docType type, File file) {
+        factory.getDocument(type, file);
     }
 
-    public void removeDocument(File file) {
-
+    public void removeDocument(Document doc) {
+        if (tabs.contains(doc)) {
+            tabs.remove(doc);
+        }
     }
 
-    public void setDirectory(Folder) {
-
+    public void setDirectory(Folder folder) {
+        currentDirectory = folder;
     }
 
     public Folder getDirectory(Folder) {
-
+        return currentDirectory;
     }
-*/
 }
