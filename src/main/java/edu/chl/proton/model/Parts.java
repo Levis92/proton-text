758f896ff3fa0332c1ae3bec55bdcfb791d5f7ed
Kaffemakarn <micsod@student.chalmers.se>
2017-05-09 21:43:44 +0200

    Made small changes in Parts

--- src/main/java/edu/chl/proton/model/Parts.java
+++ src/main/java/edu/chl/proton/model/Parts.java
@@ -10,16 +10,12 @@ public class Parts {
 
     private List<Parts> parts = new ArrayList<Parts>();
     private List<Char> chars = new ArrayList<Char>();
-    private String str;
 
     public Parts(List<Parts> parts, List<Char> chars){
         this.parts = parts;
         this.chars = chars;
     }
 
-    public Parts(String str){
-        this.str = str;
-    }
 
 
 
