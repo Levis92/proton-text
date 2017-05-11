08dba183a3e7f08db526e63b91136575163a4252
Stina Werme <stina.werme@gmail.com>
2017-05-11 14:03:38 +0200

    Add test for addFile and addFolder

--- src/test/java/edu/chl/proton/model/FolderTest.java
+++ src/test/java/edu/chl/proton/model/FolderTest.java
@@ -36,5 +36,34 @@ public class FolderTest {
         assertTrue("The folder folder should not be null", folder != null);
         assertTrue("ParentFolder should be null", file.getParentFolder() == null);
     }
+
+    @Test
+    public void addFileTest() {
+
+        Folder parent = new Folder("parent");
+        File childFile = new File("childFile", parent);
+        Folder newParent = new Folder("newParent");
+
+        newParent.addFile(childFile);
+
+        assertTrue("The folder parent should not be null", parent != null);
+        assertTrue("The file childFile should not be null", childFile != null);
+        assertTrue("The folder newParent should not be null", newParent != null);
+        assertTrue("ParentFolder should be newParent", childFile.getParentFolder() == newParent);
+    }
+
+    @Test
+    public void addFolderTest() {
+
+        Folder one = new Folder("one");
+        Folder two = new Folder("two", one);
+        Folder three = new Folder("three");
+
+        two.addFolder(three);
+
+        assertTrue("The folder one should not be null", one != null);
+        assertTrue("The folder two should not be null", two != null);
+        assertTrue("The folder three should not be null", three != null);
+        assertTrue("ParentFolder should be three", two.getParentFolder() == three);
     }
 }
