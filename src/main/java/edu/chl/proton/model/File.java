30697dde19a8b4528a5aa251570e8d8f16b14f61
Kaffemakarn <micsod@student.chalmers.se>
2017-05-16 10:42:51 +0200

    Added aqcuire text in File

--- src/main/java/edu/chl/proton/model/File.java
+++ src/main/java/edu/chl/proton/model/File.java
@@ -1,8 +1,6 @@
 package edu.chl.proton.model;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -52,4 +50,41 @@ public class File extends FileSystemEntity {
     protected void remove() {
 
     }
+
+    // Aqcuires the text from the file we opened.
+    protected void aqcuireText(){
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(file.getName());
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            // Close file.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            file.getName() + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + file.getName() + "'");
+        }
+
+
+    }
+
 }
