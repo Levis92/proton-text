8be08ca5cf9f9a824976b8968be7378badbf40ee
Anton Levholm <anton.badminton@gmail.com>
2017-05-18 13:53:25 +0200

    optimize imports in MarkdownTabController

--- src/main/java/edu/chl/proton/control/MarkdownTabController.java
+++ src/main/java/edu/chl/proton/control/MarkdownTabController.java
@@ -20,8 +20,6 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 /**
