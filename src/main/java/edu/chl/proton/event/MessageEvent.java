0dcea215f89e1f41b03084f6c02ad7342bb9a9f5
Luddevig <eludvig@student.chalmers.se>
2017-05-18 13:47:59 +0200

    add event

---
+++ src/main/java/edu/chl/proton/event/MessageEvent.java
@@ -0,0 +1,13 @@
+package edu.chl.proton.event;
+
+/**
+ * Created by ludvig on 2017-05-18.
+ */
+public class MessageEvent {
+
+    public final String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+}
