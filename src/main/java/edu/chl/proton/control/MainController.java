ca49729ebee8de5fc8779e08d5d24aa424cbd884
Luddevig <eludvig@student.chalmers.se>
2017-05-24 12:49:37 +0200

    remove large part of FontStyle we no longer use + fix Rename

--- src/main/java/edu/chl/proton/control/MainController.java
+++ src/main/java/edu/chl/proton/control/MainController.java
@@ -176,10 +176,21 @@ public class MainController {
         String path = "./Rename.txt";
         String title = "Set new name";
         TextPrompt prompt = new TextPrompt(stage.getStage(),title,path);
-        if ((path = prompt.getResult()) != null) {
-            //skicka vidare till typ av fil?
+        while ( prompt.getResult().length() <6 ||  ((prompt.getResult()).charAt(prompt.getResult().length()-3))
+                 )
+        {
 
         }
+            if ((!path.equals(prompt.getResult())) && 1 == 1) { // check if user has written correctly
+                //Spara som, fast endast då man skriver nytt namn
+                //System.out.println(prompt.getResult());
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Write a correct file name.", ButtonType.OK);
+                alert.showAndWait();
+                prompt = new TextPrompt(stage.getStage(), title, prompt.getResult());
+            }
+
+
+
     }
 
     public void onClickSaveAs(ActionEvent actionEvent) {
