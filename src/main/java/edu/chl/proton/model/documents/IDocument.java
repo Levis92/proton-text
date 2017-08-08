f7155a845ec66b90e7059aaf2d712e9f12fede11
Luddevig <eludvig@student.chalmers.se>
2017-08-08 15:28:42 +0200

    ceate IDocument

---
+++ src/main/java/edu/chl/proton/model/documents/IDocument.java
@@ -0,0 +1,23 @@
+package edu.chl.proton.model.documents;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by ludvig on 2017-08-08.
+ */
+public interface IDocument {
+    boolean doesExist();
+    File getFile();
+    String getPath();
+    String getText();
+    void setText(List<String> text);
+    String getHTML();
+    void save(String path);
+    boolean save() throws IOException;
+    void removeFile();
+    String getDateForLastEdited();
+    boolean isSaved();
+    void aqcuireText();
+}
