bc731c569e88b05b7cb81c84ecd7d748ed060941
Kaffemakarn <micsod@student.chalmers.se>
2017-08-08 16:08:08 +0200

    Added a CheckLink class with relevant code from Markdown

---
+++ src/main/java/edu/chl/proton/model/documents/CheckLink.java
@@ -0,0 +1,91 @@
+package edu.chl.proton.model.documents;
+
+import java.util.regex.*;
+
+/**
+ * Created by Mickaela on 2017-08-08.
+ * Responsible for finding and formatting matches of the pattern group "Links".
+ */
+public class CheckLink implements ICheckPattern {
+    /**
+     * Compiles a pattern and returns it. Throws
+     * PatternSyntaxException if it fails.
+     * @param pattern
+     * @return the specified pattern
+     */
+    @Override
+    public Pattern pattern(String pattern) {
+        Pattern textLink;
+        Pattern picLink;
+        try{
+            textLink = Pattern.compile("(?<!\\!)\\[(?<text>[^\\]]*)\\]\\((?<link>[^\\)]*)\\)");
+            picLink = Pattern.compile("\\!\\[(?<text>[^\\]]*)\\]\\((?<link>[^\\)]*)\\)");
+        } catch (PatternSyntaxException ex){
+            throw(ex);
+        }
+
+        if(pattern.equals("textLink")){
+            return textLink;
+        } else if(pattern.equals("picLink")){
+            return picLink;
+        }
+        return null;
+    }
+
+    /**
+     * Adds styling for the preview window
+     * @param text
+     * @return the string with the proper HTML styling
+     */
+    @Override
+    public String checkPattern(String text) {
+        // Check for img link
+        Matcher match = pattern("picLink").matcher(text);
+        StringBuffer sb = new StringBuffer();
+        while (match.find()) {
+            match.appendReplacement(sb, "<img style=\"max-width:100%\" src=\"$2\" alt=\"$1\">");
+        }
+        match.appendTail(sb);
+        String tmp = sb.toString();
+
+        // check for text link
+        match = pattern("textLink").matcher(tmp);
+        sb = new StringBuffer();
+        while(match.find()){
+            match.appendReplacement(sb, "<a href=\"$2\">$1</a>");
+        }
+        match.appendTail(sb);
+        tmp = sb.toString();
+        return tmp;
+    }
+
+    /**
+     * Adds styling for the editor window
+     * @param text the string to be formatted
+     * @param style the style to be applid to the string
+     * @return the formatted string
+     */
+    @Override
+    public String checkPattern(String text, FontStyle style) {
+        String tmp = text;
+        // Check for img link
+        Matcher match = pattern("picLink").matcher(tmp);
+        StringBuffer sb = new StringBuffer();
+        while (match.find()) {
+            match.appendReplacement(sb, style.getLinkStyle(match.group(0)));
+        }
+        match.appendTail(sb);
+        tmp = sb.toString();
+
+        // check for text link
+        match = pattern("textLink").matcher(tmp);
+        sb = new StringBuffer();
+        while (match.find()) {
+            match.appendReplacement(sb, style.getLinkStyle(match.group(0)));
+        }
+        match.appendTail(sb);
+        tmp = sb.toString();
+
+        return tmp;
+    }
+}
